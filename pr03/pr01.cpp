6bda0fbb4e505a22462bcd36475805cbf61b1022
Junqi Yang <junqiyang@linux2.cs.tamu.edu>
2018-10-07 18:06:33 -0500

    new file:   pr03/00.ppm 	new file:   pr03/2.ppm 	modified:   pr03/pr01.cpp 	modified:   pr03/pr02

--- pr03/pr01.cpp
+++ pr03/pr01.cpp
@@ -34,18 +34,27 @@
 #include <cassert>
 #include <sstream>
 #include <string>
-
+//Junqi Yang
+//221004901
 
 using namespace std;
 // =============================================================================
 // These variables will store the input ppm image's width, height, and color
 // =============================================================================
-int width, height, max_color;
+int width, height, max_color, curveW, curveH, level;
+int cxLevel = 0;
+int CY[255];
 unsigned char *pixmap;
 unsigned char *pixrep;
 float I = 0, J = 0;
 int r = 0,g = 0,b=0;
 int neig = 4;
+int N = 4;
+
+
+
+
+
 
 
 
@@ -125,7 +134,6 @@ static void init(void)
   glClearColor(1,1,1,1); // Set background color.
 }
 
-
 void load(string filename){
     FILE *input_file = fopen(filename.c_str(), "r");    
     //assume we will have p6 ppm as input
@@ -164,7 +172,6 @@ void load(string filename){
     }
     fclose(input_file);
 }
-
 
 void load_re(string filename){
     FILE *input_file = fopen(filename.c_str(), "r");    
@@ -205,25 +212,6 @@ void load_re(string filename){
     fclose(input_file);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 void RGBtoHSV(int r, int g, int b, double &h, double &s, double &v){
   double red, green, blue; 
   double max, min, delta;
@@ -253,7 +241,6 @@ void RGBtoHSV(int r, int g, int b, double &h, double &s, double &v){
     } 
   } 
 } 
-
 
 void HSVtoRGB(int &r, int &g, int &b, double h, double s, double v){
 int i;
@@ -301,20 +288,6 @@ int i;
 	}    
 } 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 void replaceColor(string sourcefile, string replacefile)
 {
 
@@ -359,24 +332,181 @@ void replaceColor(string sourcefile, string replacefile)
 
 
 
+class Vector2f{
+    public:    
+    float x, y;
+    
+    Vector2f() {}
+    Vector2f( float _x, float _y ) { x=_x; y=_y; }    
+    Vector2f operator+(const Vector2f &v) const  { return (Vector2f(x + v.x, y + v.y));    }
+    Vector2f operator-(const Vector2f &v) const  { return (Vector2f(x - v.x, y + v.y));    }
+    
+    
+    Vector2f operator*(float t) const  { return (Vector2f(x * t, y * t));} 
+    Vector2f operator /(float t) const
+    {
+        float f = 1.0F / t;
+        return (Vector2f(x * f, y * f));
+    }
+};
 
 
 
 
 
 
-
-
-class Point2f{
+class Point2f: public Vector2f{
     public:
     float x, y;
     Point2f() {}
     Point2f( float _x, float _y ) { x=_x; y=_y; }
     Point2f operator+( const Point2f &pt ) const { return Point2f(x+pt.x, y+pt.y); }    
     Point2f operator-( const Point2f &pt ) const { return Point2f(x-pt.x, y-pt.y); }
+    Point2f operator*(float t) const  { return (Point2f(x * t, y * t)); }
+    Point2f operator/(float t) const { float f = 1.0F / t; return (Point2f(x * f, y * f));}
+    
+    Point2f& operator =(const Vector2f& v)
+    {
+        x = v.x;
+        y = v.y;
+        return (*this);
+    }
+    
+    Point2f operator +(const Vector2f& v) const
+    {
+        return (Point2f(x + v.x, y + v.y));
+    }
+    
+    
+    
     float Dot	   ( const Point2f &pt ) const { return x*pt.x + y*pt.y; }	///< Dot product
 	float operator%( const Point2f &pt ) const { return Dot(pt); }
+    
 };
+
+
+
+
+
+
+
+
+Point2f F(Point2f p0, Point2f p1, Vector2f v0, Vector2f v1, float t)
+{
+    float h0 = 2.0*powf(t,3) - 3.0*powf(t,2) +1;
+    float h1 = -2.0*powf(t,3) + 3.0*powf(t,2);
+    float h2 = powf(t,3) - 2.0*powf(t,2) + t;
+    float h3 = powf(t,3) - powf(t,2);
+    Point2f Ft;
+    Ft = p0*h0 + p1*h1 + v0*h2 + v1*h3;
+    return Ft;
+}
+
+
+
+void getColor(int mode){
+    for(int y = 0; y < height ; y++) {
+        for(int x = 0; x < width; x++) {
+            int i = (y * width + x) * 3;
+            if (mode == 0 || mode == 1) {
+                pixmap[i] = CY[pixmap[i]];
+            }
+            i++;
+            if (mode == 0 || mode == 2) {
+                pixmap[i] = CY[pixmap[i]];
+            }
+            i++;
+            if (mode == 0 || mode == 3) {
+                pixmap[i] = CY[pixmap[i]];
+            }
+        }
+    }
+}
+
+void colormap(Point2f p0, Point2f p1){
+    int CX0, CX1;
+    float cx0, cx1, cy0, cy1;
+    p0 = p0;
+    p1 = p1;   
+    
+    cx0 = p0.x;
+    cx1 = p1.x;
+    cy0 = p0.y;
+    cy1 = p1.y;   
+    
+    CX0 = (int)(cx0 + 0.5);
+    CX1 = (int)(cx1 + 0.5); 
+
+    for (int CX = CX0; CX < CX1; CX++){
+        float t = CX;
+        int CYtemp = (int)(((1-t) *cy0 + t * cy1) + 0.5);
+        CY[cxLevel] = CYtemp;
+        cxLevel++;
+    }    
+}
+
+
+void create_curve(Point2f p[]){
+    Vector2f v[N+1];
+    Point2f P0, P1;
+    v[0] = p[1] - p[0];
+    for (int i = 1; i<N; i++) {
+        v[i] = (p[i+1] - p[i-1])/2.0;
+    }
+    v[N] = p[N] - p[N-1];
+    P0 = F(p[0], p[1], v[0], v[1], 0);
+    for (int i = 0; i< N; i++) {
+        for( int T=1; T<100; T++)
+        {
+            float t = (float)T/(100 - 1);
+            P1 = F(p[i], p[i+1], v[i], v[i+1],t);
+            colormap(P0, P1);
+            P0 = P1;
+        }
+    }
+}
+
+
+
+
+void curve(int mode){
+    Point2f p[N+1];
+
+    p[0] = Point2f(0, 0);
+    p[1] = Point2f(0.1, 0.2);
+    p[2] = Point2f(0.5, 0.8);
+    p[3] = Point2f(0.7, 0.7);
+    p[4] = Point2f(1, 1); 
+    create_curve(p);  
+    getColor(mode);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Point3f{
     public:
@@ -398,8 +528,8 @@ int main(int argc, char *argv[])
 {
 
   //initialize the global variables
-	width = 600;
-	height = 600;
+  width = 600;
+  height = 600;
   
 
   // OpenGL Commands:
@@ -410,8 +540,16 @@ int main(int argc, char *argv[])
   glutInitWindowSize(width, height);
   glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
   glutCreateWindow("Homework Zero");
-  replaceColor("shaded.ppm", "sp.ppm");
+  
+  int option;
+  cout<<"1.Curve-Red \n"
+  
+  
+//  replaceColor("shaded.ppm", "sp.ppm");
+  load("00.ppm");
   init();
+  curve(1);
+  save("2.ppm");
   glutReshapeFunc(windowResize);
   glutDisplayFunc(windowDisplay);
   glutMouseFunc(processMouse);
